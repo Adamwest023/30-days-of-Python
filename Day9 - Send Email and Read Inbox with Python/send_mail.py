919f3b5c6f3afd1a94f901d163bdfb440311736f
Adam West <Adamwest023@gmail.com>
2022-12-16 09:30:13 -0600

    added email script and enviormental variables

---
+++ Day9 - Send Email and Read Inbox with Python/send_mail.py
@@ -0,0 +1,39 @@
+import smtplib
+import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+
+# environment variables
+load_dotenv()
+
+username = 'adamsboggusacct@gmail.com'
+password = os.getenv("PASSWORD")
+
+
+
+def send_mail(text="Email body", subject="Hello, world!",
+               from_email='Adam <adamsboggusacct@gmail.com>',
+               to_emails=None,):
+    assert isinstance(to_emails, list)
+    
+    msg = MIMEMultipart('alternative')
+    msg['From'] = from_email
+    msg['To'] = ", ".join(to_emails)
+    msg['Subject'] = subject
+    
+    txt_part = MIMEText(text,'plain')
+    msg.attach(txt_part)
+    html_part = MIMEText("<h1>This is working</h1>",'html')
+    # msg.attach(html_part)
+    
+    msg_str = msg.as_string()
+    
+    # login to my smtp server
+    server = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    server.ehlo()
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(from_email,to_emails, msg_str)
+
+    server.quit()
