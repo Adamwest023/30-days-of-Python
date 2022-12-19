91f02ce0899ea2e2ebbbe3c3e93a7f2d5cd3faac
Adam West <Adamwest023@gmail.com>
2022-12-19 11:42:19 -0600

    added classes

--- Day 11/animals.py
+++ Day 11/animals.py
@@ -1,4 +1,22 @@
 class Animal:
     noise = "Rrr"
-    def mke_noise(self):
-        print("hello")
+    color = "Red"
+    def make_noise(self):
+        print(f"{self.noise}")
+    def set_noise(self, new_noise):
+        self.noise = new_noise
+        
+    def get_noise(self):
+        return self.noise
+    def set_noise(self):
+        self.new_noise = new_noise
+        return self.noise
+
+    def get_color(self):
+        return self.color
+    def set_color(self):
+        self.new_color = new_color
+        return self.color
+        
+class Wolf(Animal):
+    noise = "grrr"
