cbfae6a2589f283556d6e3f954a34f8ed85e869a
Adam West <Adamwest023@gmail.com>
2022-12-21 11:45:23 -0600

    created panda dataframe for project

--- Day 13/connect.py
+++ Day 13/connect.py
@@ -1,6 +1,7 @@
 import os
 import pprint
 import requests
+import pandas as pd
 from dotenv import load_dotenv
 
 # Environmental variables
@@ -16,13 +17,13 @@ movie_id = 500
 api_version = 3
 api_base_url = f"https://api.themoviedb.org/{api_version}"
 endpoint_path = f"/movie/{movie_id}"
-endpoint = f"{api_base_url}{endpoint_path}?api_key={api_key}"
+endpoint = f"{api_base_url}{endpoint_path}?api_key={api_key}&page=1"
 print(endpoint)
 r = requests.get(endpoint)
 print(r.status_code)
 print(r.text)
 
-#Get Search movie
+# Get Search movie
 api_base_url = f"https://api.themoviedb.org/{api_version}"
 endpoint_path = f"/search/movie"
 search_query = "The Matrix"
@@ -30,7 +31,7 @@ endpoint = f"{api_base_url}{endpoint_path}?api_key={api_key}&query={search_query
 # print(endpoint)
 r = requests.get(endpoint)
 # pprint.pprint(r.json())
-if r.status_code in range (200,299):
+if r.status_code in range(200, 299):
     data = r.json()
     results = data['results']
     if len(results) > 0:
@@ -38,11 +39,28 @@ if r.status_code in range (200,299):
         movie_ids = set()
         for result in results:
             _id = result['id']
-            print(result['title'], _id)
+            # print(result['title'], _id)
             movie_ids.add(_id)
         # print(list(movie_ids))
 
-#using v4
+output = 'movies.csv'
+movie_data = []
+for movie_id in movie_ids:
+    api_version = 3
+    api_base_url = f"https://api.themoviedb.org/{api_version}"
+    endpoint_path = f"/movie/{movie_id}"
+    endpoint = f"{api_base_url}{endpoint_path}?api_key={api_key}"
+    r = requests.get(endpoint)
+    if r.status_code in range(200, 299):
+        data = r.json()
+        movie_data.append(data)
+
+# pandas Dataframe
+df = pd.DataFrame(movie_data)
+print(df.head())
+df.to_csv(output, index=False)
+
+# using v4
 """
 movie_id = 501
 api_version = 4
