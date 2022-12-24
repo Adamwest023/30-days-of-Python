8171463dbed2a4bafef90e400a81be3ea2b81e4a
Adam West <Adamwest023@gmail.com>
2022-12-24 12:38:35 -0600

    completed tutorial with moviepy's frame grab

--- Day 15/one_thumbs.py
+++ Day 15/one_thumbs.py
@@ -5,7 +5,12 @@ from PIL import Image
 
 source_path = os.path.join(SAMPLE_INPUTS, "sample.mp4")
 thumbnail_dir = os.path.join(SAMPLE_OUTPUTS, "thumbnails")
+thumbnail_per_frame_dir = os.path.join(SAMPLE_OUTPUTS, "thumbnails-per-frame")
+thumbnail_per_half_second_dir = os.path.join(SAMPLE_OUTPUTS, "thumbnails-per-half-second")
+
 os.makedirs(thumbnail_dir, exist_ok=True)
+os.makedirs(thumbnail_per_frame_dir, exist_ok=True)
+os.makedirs(thumbnail_per_half_second_dir, exist_ok=True)
 
 clip = VideoFileClip(source_path)
 print(clip.reader.fps)
@@ -13,12 +18,38 @@ print(clip.reader.nframes)
 print(clip.duration)  # in seconds
 
 
+# based of duration
 duration = clip.duration  # clip.reader.duration
-max_duration = int(duration) +1
+max_duration = int(duration) + 1
 for i in range(0, max_duration):
     frame = clip.get_frame(int(i))
     # print(frame) #np.array numpy array # inference
-    new_img_filepath = os.path.join(thumbnail_dir, f"{i}.jpg") 
+    new_img_filepath = os.path.join(thumbnail_dir, f"{i}.jpg")
     print(f'frame at {i} seconds saved at {new_img_filepath}')
     new_img = Image.fromarray(frame)
     new_img.save(new_img_filepath)
+
+fps = clip.reader.fps
+nframes = clip.reader.nframes
+seconds = nframes / (fps*1.0)
+
+# based of frames
+for i, frame in enumerate(clip.iter_frames()):
+    # print(frame) #np.array numpy array # inference
+    if i % fps == 0:
+        current_ms = int((i/fps)*1000)
+        new_img_filepath = os.path.join(
+            thumbnail_per_frame_dir, f"{current_ms}ms.jpg")
+        # print(f'frame at {i} seconds saved at {new_img_filepath}')
+        new_img = Image.fromarray(frame)
+        new_img.save(new_img_filepath)
+
+
+#for half seconds
+for i, frame in enumerate(clip.iter_frames()):
+    fphs = int(fps/2.0)
+    if i % fphs == 0:
+        current_ms = int((i/fps)*1000)
+        new_img_filepath = os.path.join(thumbnail_per_half_second_dir,f'{current_ms}ms.jpg')
+        new_img = Image.fromarray(frame)
+        new_img.save(new_img_filepath)
